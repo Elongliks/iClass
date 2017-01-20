7612a1cf4b22465041126b35e13f7e293c2096d7
Elon Gliksberg <Elon Gliksberg>
2017-01-20 19:24:41 +0200

    Added Android Index

--- index.android.js
+++ index.android.js
@@ -1,53 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Navigator } from 'react-native';
+import First from './app/First';
+import Second from './app/Second';
+import Third from './app/Third';
 
 export default class iClass extends Component {
+  navigatorRenderScene(route, navigator) {
+    switch (route.id) {
+      case 'First':
+      return <First navigator={navigator} />
+      case 'Second':
+      return <Second navigator={navigator} />
+      case 'Third':
+      return <Third navigator={navigator} />
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator initialRoute={{id: 'First'}} renderScene={this.navigatorRenderScene} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('iClass', () => iClass);
